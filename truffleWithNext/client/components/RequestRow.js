2a5554289aef7c1c1f21a1b6ca2c9fff1f3d1de5
alinasolinas <azayneeva@gmail.com>
2018-08-19 13:06:27 +0100

    added functionality to approve and finalize requests

---
+++ truffleWithNext/client/components/RequestRow.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import {Table, Button, Message} from 'semantic-ui-react';
+import Campaign from '../lib/contracts/Campaign.json';
+
+class RequestRow extends Component {
+    state = {
+        approveErrorMessage: '',
+        finalizeErrorMessage: ''
+    }
+
+    onApprove = async () => {
+        const {address, id, web3} = this.props;
+        this.setState({
+            approveErrorMessage: '',
+            finalizeErrorMessage: ''
+        })
+        try {
+            const accounts = await web3.eth.getAccounts();
+            const campaign = await new web3.eth.Contract(Campaign.abi, address);
+            await campaign.methods
+                .approveRequest(id)
+                .send({
+                    from: accounts[0]
+                })
+        } catch (error) {
+            this.setState({
+                approveErrorMessage: error.message
+            })
+        }
+    };
+
+    onFinalize = async () => {
+        const {address, id, web3} = this.props;
+        this.setState({
+            approveErrorMessage: '',
+            finalizeErrorMessage: ''
+        })
+        try {
+            const accounts = await web3.eth.getAccounts();
+            const campaign = await new web3.eth.Contract(Campaign.abi, address);
+            await campaign.methods
+                .finalizeRequest(id)
+                .send({
+                    from: accounts[0]
+                })
+        } catch (error) {
+            this.setState({
+                finalizeErrorMessage: error.message
+            })
+        }
+    };
+
+    render() {
+        const {Row, Cell} = Table;
+        const {id, request, web3, approversCount} = this.props;
+        const readyToBeFinalized = request.approvalCount > approversCount/2;
+        return (
+            <Row disabled={request.complete} positive={readyToBeFinalized && !request.complete}>
+                <Cell>{id}</Cell>
+                <Cell>{request.description}</Cell>
+                <Cell>{`${web3.utils.fromWei(request.value, 'ether')} ether`}</Cell>
+                <Cell>{request.recipient}</Cell>
+                <Cell>{`${request.approvalCount}/${approversCount}`}</Cell>
+                <Cell>
+                    {!request.complete &&
+                        <Button color='green' basic onClick={this.onApprove}>Approve</Button>
+                    }
+                    {!!this.state.approveErrorMessage && <Message error>{this.state.approveErrorMessage}</Message>}
+                </Cell>
+                <Cell>
+                    {!request.complete &&
+                        <Button primary basic onClick={this.onFinalize}>
+                            Finalize
+                        </Button>
+                    }
+                    {!!this.state.finalizeErrorMessage && <Message error>{this.state.finalizeErrorMessage}</Message>}
+                </Cell>
+            </Row>
+        )
+    }
+};
+
+export default RequestRow;
--- truffleWithNext/client/pages/campaigns/requests/index.js
+++ truffleWithNext/client/pages/campaigns/requests/index.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-import {Button} from 'semantic-ui-react';
+import {Button, Table} from 'semantic-ui-react';
 import {Link} from '../../../routes';
 import Web3Container from '../../../lib/Web3Container';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../lib/contracts/Campaign.json';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
     state = {
         requests: null,
-        requestCount: ''
+        requestCount: '',
+        approversCount: ''
     };
 
     async componentWillMount() {
@@ -20,22 +22,56 @@ class RequestIndex extends Component {
                 return campaign.methods.requests(index).call()
             })
         );
-
+        const approversCount = await campaign.methods.approversCount().call();
         this.setState({
             requests,
-            requestCount
+            requestCount,
+            approversCount
         });
     };
 
+    renderRows = () => {
+        if (this.state.requests) {
+            return this.state.requests.map(((request, index) => {
+                return <RequestRow 
+                    key={index}
+                    id={index}
+                    request={request}
+                    web3={this.props.web3}
+                    approversCount={this.state.approversCount}
+                    address={this.props.address}
+                />
+            }))
+        }
+    };
+
     render() {
+        const {Header, Row, HeaderCell, Body} = Table;
         return (
             <Layout>
                 <h3>Requests</h3>
                 <Link route={`/campaigns/${this.props.address}/requests/new`}>
                     <a>
-                        <Button primary>Add Request</Button>
+                        <Button primary floated='right' style={{marginBottom: 10}}>Add Request</Button>
                     </a>
                 </Link>
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval Count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                    <Body>
+                        {this.renderRows()}
+                    </Body>
+                </Table>
+                <div>Found {this.state.requestCount} requests</div>
             </Layout>
         )
     }
