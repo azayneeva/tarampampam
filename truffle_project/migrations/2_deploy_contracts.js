a08347281a37d35f8683403ae9621c6a6e205437
alinasolinas <azayneeva@gmail.com>
2018-08-20 21:54:20 +0100

    cleaned up the file structure

--- truffle_project/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
-var CampaignFactory = artifacts.require("CampaignFactory");
-
-module.exports = function(deployer) {
-  deployer.deploy(CampaignFactory);
-};
